acc55c2d719b91bd0eb069e6036cf6fa29c0fc2f
Ryan DeGroot <degrootr253@gmail.com>
2022-01-04 06:08:38 -0800

    fixing bugs

--- db/queryFunc.js
+++ db/queryFunc.js
@@ -11,7 +11,7 @@ class QUERY {
     return this.connection
       .promise()
       .query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+        "SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
       );
   }
 
@@ -19,7 +19,7 @@ class QUERY {
     return this.connection
       .promise()
       .query(
-        "SELECT role.id, role.title, department.name AS Department, role.salary from role LEFT JOIN department ON role.department_id=department.id;"
+        "SELECT roles.id, roles.title, department.name AS department, roles.salary from roles LEFT JOIN department ON roles.department_id=department.id;"
       );
   }
   findDepartments() {
@@ -37,7 +37,7 @@ class QUERY {
   }
   addRole(newRole) {
     console.log("hit the function!");
-    return this.connection.promise().query("Insert into role SET ?", newRole);
+    return this.connection.promise().query("Insert into roles SET ?", newRole);
   }
   addEmployee(newEmployee) {
     console.log("hit the function!");
@@ -45,8 +45,8 @@ class QUERY {
       .promise()
       .query("INSERT INTO employee SET ?", newEmployee);
   }
-  updateEmployee() {
-    return this.connection.promise().query("");
+  updateEmployee(updateEmployee) {
+    return this.connection.promise().query("UPDATE employee SET ? WHERE ?",updateEmployee);
   }
 }
 
