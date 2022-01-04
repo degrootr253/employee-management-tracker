c30e18a566352e1d2e8dde100be4e1f6aadcc3ce
Ryan DeGroot <degrootr253@gmail.com>
2022-01-04 06:12:24 -0800

    fixing query bugs

--- db/queryFunc.js
+++ db/queryFunc.js
@@ -11,7 +11,7 @@ class QUERY {
     return this.connection
       .promise()
       .query(
-        "SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+        "SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
       );
   }
 
